3c873f6cb009acdb2c0bae933943fa0824eb9751
Murilo Ferro <muri.ferro@outlook.com>
2025-06-13 21:48:29 -0300

    65001

---
+++ Eclipse/WorkSpace/conversaoGalao2Litros/src/br/com/senaisp/bauru/aula03/Exemplo02.java
@@ -0,0 +1,12 @@
+package br.com.senaisp.bauru.aula03;
+
+public class Exemplo02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Divisão entre dois inteiros, o resultado é inteiro
+		//A fraçao é desprezada!
+		System.out.println( (double) 1 / 2 + 0.2);
+	}
+
+}
